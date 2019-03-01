722a0300893aa181ba09e66ade08daeeb4715692
Pierre Goiffon <pierre.goiffon@combodo.com>
2019-03-01 16:46:35 +0100

    :sparkles: post-checkout : backup and create symlinks

--- post-checkout.iTop.php
+++ post-checkout.iTop.php
@@ -10,36 +10,74 @@
  * If backup does not exists, create empty dirs so that setup run is needed
  * If backup exists then simple symlink are created
  */
-const ITOP_DIRS = array('conf', 'data', 'env-production');
 
 
-echo "## iTop DEV Git Hook ##\n";
+const BKP_DIR = '.CONF-BKP';
+const ITOP_DIRS = array('conf', 'env-production');
 
-$repoRoot = execGitCmd('git rev-parse --show-toplevel');
-echo "repo root=$repoRoot\n";
+main();
 
 
-$repoBranchSource = execGitCmd('git rev-parse --abbrev-ref @{-1}');
-$repoBranchTarget = execGitCmd('git rev-parse --abbrev-ref head');
-
-echo "previous branch=$repoBranchSource\n";
-echo "checked out branch=$repoBranchTarget\n";
-
-foreach (ITOP_DIRS as $sCurrentDir)
+function main()
 {
-	$sCurrentPath = $repoRoot.'/'.$sCurrentDir;
+	echo "## iTop DEV Git Hook ##\n";
 
-	$bIsCurrentConfSymlink = is_link($sConfDir);
-	if (!$bIsCurrentConfSymlink)
+	$sRepoRoot = execGitCmd('git rev-parse --show-toplevel');
+	echo "repo root=$sRepoRoot\n";
+
+
+	$sSourceBranch = execGitCmd('git rev-parse --abbrev-ref @{-1}');
+	$sTargetBranch = execGitCmd('git rev-parse --abbrev-ref head');
+	echo "previous branch=$sSourceBranch\n";
+	echo "checked out branch=$sTargetBranch\n";
+
+	foreach (ITOP_DIRS as $sCurrentDir)
 	{
-		backupDir($repoRoot, $sDirName, $repoBranchSource);
+		$sCurrentPath = $sRepoRoot.'/'.$sCurrentDir;
+
+		echo "-- Path: $sCurrentPath...\n";
+		$bIsCurrentPathExists = file_exists($sCurrentPath);
+		if ($bIsCurrentPathExists)
+		{
+			$bIsCurrentPathSymlink = is_link($sCurrentPath);
+			if ($bIsCurrentPathSymlink)
+			{
+				echo 'removing symlink...';
+				// using rmdir instead of unlink as the symlink points to a directory
+                // see https://stackoverflow.com/a/18262809/5114238
+				rmdir($sCurrentPath);
+				echo 'ok !';
+			}
+			else
+			{
+				$sCurrentSourceBkpPath = getConfDirBackup($sRepoRoot, $sSourceBranch, $sCurrentDir);
+				echo "backing up to $sCurrentSourceBkpPath...";
+				@chmod($sCurrentPath, 0777); // conf might be protected !
+				rename($sCurrentPath, $sCurrentSourceBkpPath);
+				echo 'ok !';
+			}
+		}
+		else
+		{
+			echo 'doesn\'t exist !';
+		}
+		echo "\n";
+
+		$sCurrentTargetBkpPath = getConfDirBackup($sRepoRoot, $sTargetBranch, $sCurrentDir);
+		echo "target $sCurrentTargetBkpPath...";
+		if (!file_exists($sCurrentTargetBkpPath))
+		{
+			echo 'creating...';
+			mkdir($sCurrentTargetBkpPath);
+			echo 'ok !';
+		}
+		echo "\n";
+
+		// create symlink
+		echo 'creating symlink...';
+		symlink($sCurrentTargetBkpPath, $sCurrentPath);
+		echo "ok !\n";
 	}
-
-	// target exists ?
-	// N : create
-	// Y : get path
-
-	// create/update symlink
 }
 
 
@@ -57,16 +95,40 @@ function execGitCmd($sGitCommand)
 
 
 /**
- * Move dir as backup (adds prefix)
- *
  * @param  string $sRepoRoot
- * @param  string $sDirName
  * @param  string $sBranchName
+ * @param  string $sDirName
+ *
+ * @return string
  */
-function backupDir($sRepoRoot, $sDirName, $sBranchName)
+function getConfDirBackup($sRepoRoot, $sBranchName, $sDirName)
 {
-	$sBranchNamePath = str_replace('/', '--', $sBranchName);
-	$sDirSourcePath = $sRepoRoot.'/'.$sDirName;
-	$sDirBackupPath = $sRepoRoot.'/.BKP-'.$sBranchNamePath.'.'.$sDirName;
-	rename($sDirSourcePath, $sDirBackupPath);
+	$sConfBkpPath = $sRepoRoot.'/'.BKP_DIR;
+	if (!file_exists($sConfBkpPath))
+	{
+		mkdir($sConfBkpPath);
+	}
+
+	$sBranchNameForPath = getBranchNameForPath($sBranchName);
+	$sBranchBkpPath = $sConfBkpPath.'/'.$sBranchNameForPath;
+	if (!file_exists($sBranchBkpPath))
+	{
+		mkdir($sBranchBkpPath);
+	}
+
+	$sDirBackupPath = $sBranchBkpPath.'/'.$sDirName;
+
+	return $sDirBackupPath;
+}
+
+/**
+ * @param string $sBranchName
+ *
+ * @return string
+ */
+function getBranchNameForPath($sBranchName)
+{
+	$sBranchNameForPath = str_replace('/', '--', $sBranchName);
+
+	return $sBranchNameForPath;
 }
